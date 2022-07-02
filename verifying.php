25dcf9c8a383cefe5ac98c8dc3b630311a208ce3
MuhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2022-07-03 01:21:17 +0200

    first commit - login page

---
+++ verifying.php
@@ -0,0 +1,29 @@
+<?php
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $radio = $_POST['radio'];
+    // echo "$email"."<br>";
+    // echo "$pass"."<br>";
+    // echo "$who"."<br>";
+
+    $connection = new PDO ("mysql:host=localhost;dbname=cafeteria", "root", "");
+    if ($radio == "admin") {
+        $data = $connection->query("select email, password from admin where email = '$email' and password = '$pass'");
+        $result = $data->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($result);
+        if (count($result) == 0) {
+            echo "email or password is incorrect <br>";
+        }
+        else {header("location:admin.php");}
+    }
+
+    elseif ($radio == "user") {
+        $data = $connection->query("select email, password from users where email = '$email' and password = '$pass'");
+        $result = $data->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($result);
+        if (count($result) == 0) {
+            echo "email or password is incorrect";
+        }
+        else {header("location:users.php");}
+    }
+?>
