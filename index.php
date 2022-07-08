d9f398aaea48d7a56306359840d252eb7113e50d
mohamed_samir <86778943+mohammed-samir01@users.noreply.github.com>
2022-07-08 16:56:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- head Section -->
+<?php include "layouts/header.php" ?>
+<!-- End of head Section -->
+
+<!-- header section -->
+<?php include "layouts/header_menu.php" ?>
+<!-- End of header section -->
+
+<!-- Sidbar Section -->
+<?php include "layouts/side_menubar.php" ?>
+<!-- End Of Sidbar Section -->
+
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+
+        <?php include "layouts/path.php";?>
+        <?php include "layouts/head.php";?>
+
+        <!-- /.row -->
+    </section>
+    </div>
+
+    <!-- Footer Section -->
+<?php include "layouts/footer.php" ?>
+<!-- End Of Footer Section -->
+
+<?php include "layouts/scripts.php" ?>
